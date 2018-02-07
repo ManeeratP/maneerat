f9a03d458ec612d23d2f1d2fd694462ce1c53ffe
ManeeratP <36217346+ManeeratP@users.noreply.github.com>
2018-02-07 11:05:20 +0700

    Add files via upload

---
+++ verify.php
@@ -0,0 +1,17 @@
+<?php
+$access_token = 'Ehn3yoyNXL38qDYJ3dvIoOYLY3JTiJ4t52KhbqWEv/NY44uFEeklU38krlEpKBQokw2B2Z4cRV5G6ixEHjYHjbP9ojsh7Fe27ZOumXTqyKOs/VzZE78U6xeEnEw8jFd0xRApwKtUk0cYwCuoZS7LigdB04t89/1O/w1cDnyilFU=
+';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+?>
